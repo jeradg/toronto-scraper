a8bca9c58674fb79f61d8bfaeecaa24be74c390d
Jerad Gallinger <jerad@jeradgallinger.ca>
2014-04-29 09:21:14 -0400

    Moved some files to subfolders. Started writing method to send an email when the crawler has been run. Added filters to requestURL.js.

--- index.js
+++ index.js
@@ -5,9 +5,20 @@
 // mongoose.set( 'debug', true );
 
 var path = require( 'path' ),
+    argv = require( 'minimist' )( process.argv.slice( 2 ), {
+    // User can pass an optional -e/--email flag when executing
+    // from the command line. If present, the update module will
+    // send a summary of 
+      alias: {
+        e: 'email'
+      },
+      default: {
+        e: false
+      }
+    } ),
     swimTO = require( './lib/swimTO' ),
     urlsList = path.resolve( __dirname, 'venueListURLs.json' );
 
 module.exports = swimTO;
 
-swimTO().update( urlsList );
+swimTO().update( urlsList, { email: argv.e } );
