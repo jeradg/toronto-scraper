a8a979e507bae61d6e08e353ec4f7015a4979eec
Jerad Gallinger <jerad@jeradgallinger.ca>
2014-06-18 05:38:49 -0400

    Updated updateMetadata. Added better argv parsing to index.js.

--- index.js
+++ index.js
@@ -4,23 +4,58 @@
 
 var path = require( 'path' ),
     fs = require( 'fs' ),
+    minimist = require( 'minimist' ),
+    mongoose = require( 'mongoose' ),
+    database = require( './lib/database' ),
     scraper = require( './lib/scraper' );
+    updateMetadata = require( './lib/updateMetadata' );
 
 module.exports = scraper;
 
 if ( require.main === module ) {
-  // If run from the command line, accepts one optional argument,
-  // the path to a JSON file containing an array of URLs.
-  // If no arguments given, uses a default path.
-  var args = process.argv.slice( 2 ),
-      indexURLsPath = args[ 0 ] ? args[ 0 ] : path.resolve( __dirname, 'venueListURLs.json' ),
-      urlsList = JSON.parse( fs.readFileSync( indexURLsPath ) );
+  // If run from the command line, accepts arguments:
+  // `-p [path]` / `--path [path]` (optional): the path to a 
+  //     JSON file containing an array of URLs.
+  //     (If no path is provided, uses a default path.)
+  // `-m` / '--meta' (optional): If true, only runs updateMetadata()
 
-  scraper( urlsList, function( err ) {
-    if ( err ) {
-      return console.log( 'Scrape failed with the following error: ', err );
-    } else {
-      return console.log( 'Scrape completed with no errors.' );
+  var argv = minimist( process.argv.slice( 2 ), {
+    alias: {
+      "p": "path",
+      "m": "meta"
     }
   } );
+
+  if ( argv.meta ) {
+    mongoose.connect( database );
+
+    mongoose.connection.on( 'error', function( err ) {
+      return console.log( err );
+    } );
+
+    mongoose.connection.on( 'open', function() {
+      updateMetadata( function( err ) {
+        if ( err ) {
+          mongoose.connection.close( function() {
+            return console.log( err );
+          } );
+        } else {
+          mongoose.connection.close( function() {
+            return false;
+          } );
+        }
+      } );
+    } );
+  } else {
+    var indexURLsPath = argv.path ? argv.path : path.resolve( __dirname, 'venueListURLs.json' ),
+        urlsList = JSON.parse( fs.readFileSync( indexURLsPath ) );
+
+    scraper( urlsList, function( err ) {
+      if ( err ) {
+        return console.log( 'Scrape failed with the following error: ', err );
+      } else {
+        return console.log( 'Scrape completed with no errors.' );
+      }
+    } );
+  }
 }
